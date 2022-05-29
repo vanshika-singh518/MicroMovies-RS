2d117a9d311baa8fc5770668adbd6ef56a4f0f18
Vanshika Singh Gour <83200847+vanshika-singh518@users.noreply.github.com>
2022-05-29 22:56:49 +0530

    Update recommenderapi.js

--- react-movie/src/api/recommenderapi.js
+++ react-movie/src/api/recommenderapi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://micro-movies.herokuapp.com/",
+  //baseURL: "https://micro-movies.herokuapp.com/",
   
-  //baseURL: "http://127.0.0.1:5000", 
+  baseURL: "http://127.0.0.1:5000", 
   headers: { "Content-Type": "multipart/form-data" },
 });
